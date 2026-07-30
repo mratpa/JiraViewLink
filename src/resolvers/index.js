6113386e2c0e11b0106a27e3a2590ff80398e8b0
Madhav <madhav_acharya@hotmail.com>
2026-07-30 10:56:52 -0400

    update display

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -4,7 +4,7 @@ const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
   console.log(req);
-  return '';
+  return 'Hello';
 });
 
 export const handler = resolver.getDefinitions();
